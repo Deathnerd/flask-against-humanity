e2c34121ad9ee63010ffd368db52be21f9802bdc
Wes Gilleland <wgilleland@elinkdesign.com>
2015-06-05 12:56:52 -0400

    Removed the flask task

--- api/gulpfile.js
+++ api/gulpfile.js
@@ -2,16 +2,7 @@
  * Created by deathnerd on 6/1/15.
  */
 var gulp = require('gulp');
-var shell = require('gulp-shell');
 var browserSync = require('browser-sync').create();
-
-// start up the flask server
-gulp.task('flask', shell.task([
-        'source env/bin/activate',
-        'python runserver.py server'
-        ].join(' && ')
-    )
-);
 
 // start the browserSync instance
 gulp.task('browserSync', function(){
